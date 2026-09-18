0ad3c88e7666bc10d375b6ceac1180f97bc4d547
elmagodehoja <daniplay1011@gmail.com>
2026-09-18 11:15:48 +0200

    commit dia 18-09-2026

---
+++ apuntes/00_tema_introduccion/ejercicio1.js
@@ -0,0 +1,46 @@
+// Ejercicio 1 de Javascript
+//
+
+/*
+console.log("Hola mundo")
+
+//Tipos de datos en JS
+//
+//
+// String
+// ' ' " " ` ` Comillas francesas
+// var let const
+let nombre = "Daniel"
+let apellidos = "RC"
+let aniosTrabajo = 6
+console.log(`Hola a todos, me llamo ${nombre}, ${apellidos} y llevo trabajando ${aniosTrabajo}`)
+console.log(typeoff(String(aniosTrabajo)))
+console.log(typeoff(Number(apellidos)))
+
+// validaciones básicas == ===
+//
+// == significa si el valor de la izquierda es igual al valor de la derecha
+// === significa si el valor de la izquierda coincido con el valor y tipo de la derecha
+'5' == 5  //true
+'5' === 5 //false
+
+//ternarias evaluacion de expresion ? verdadera : false
+//
+const edad = "18""
+edad < 18 ? console.log("Eres mayor de edad") : console.log("Eres menor de edad")
+
+Dada la edad, los minutos y los segundos. Comprobar primero si la edad es un numero positivo y mayor que 18 y segundo, comprobar si la hora o los minutos son valores validos en mi sistema de numeración
+
+if(${edad}>0){
+  console.log("Es positivo")
+} else{
+  console.log("Es negativo")
+}
+
+if(${edad}>=18){
+  console.log("Es mayor de edad")
+} else{
+  console.log("Es menor de edad")
+}
+
+*/
